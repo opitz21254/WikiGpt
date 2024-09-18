e58c15dd5c5b484047c0e59960a34b8dfad8dcae
Brennan Opitz <brennan.opitz@students.snow.edu>
2024-09-17 23:34:11 -0600

    Nite

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PasswordGenerator;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+
+namespace YouTubeBot
+{
+    internal class Program
+    {
+        private static DiscordClient { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
